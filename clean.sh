54fdbb1180a0b2ca9ab4ae64fa43e5a915ae6e76
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2023-12-04 10:08:43 +0800

    :sparkles: 添加开发和生产环境启动脚本
    1. `prod.sh` 构建前端文件并启动服务端
    2. `dev.sh` 同时运行 Trunk 和服务端，使用 `cargo-watch` 监视代码变动，自动重新加载前后端
    3. `clean.sh` 清理构建文件
    4. `.gitignore` 添加 Trunk 构建目录 `target-trunk`

---
+++ clean.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+rm -rf target
+rm -rf target-trunk
+rm -rf dist
---
+++ prod.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+pushd frontend
+CARGO_TARGET_DIR=../target-trunk trunk build --release --public-url /
+popd
+
+cargo run --bin backend --release
