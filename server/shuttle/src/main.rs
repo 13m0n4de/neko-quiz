2dcd62f0c619086bcc453ce5149ba9c15ac21e94
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-01-15 22:07:01 +0800

    :truck: 将 `shuttle` 使用的前端静态文件路径改为 `shuttle-dist/`

--- server/shuttle/src/main.rs
+++ server/shuttle/src/main.rs
@@ -2,6 +2,6 @@ use api::build_router;
 
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let app = build_router("config.json", "./dist/");
+    let app = build_router("config.json", "./shuttle-dist/");
     Ok(app.into())
 }
