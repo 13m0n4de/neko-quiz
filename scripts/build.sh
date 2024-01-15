c8459ccb9b4fc94efa30bc168bd4dbd629cdfe9b
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-01-15 12:00:03 +0800

    :hammer: 编译脚本支持自定义 target

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
+
 set -euo pipefail
 IFS=$'\n\t'
+
+TARGET=${1:-}
 
 pushd frontend
 CARGO_TARGET_DIR=../target-trunk trunk build --release --public-url /
 popd
 
-cargo build --bin backend --release
+if [[ -n "$TARGET" ]]; then
+    cargo build --target "$TARGET" --bin backend --release
+else
+    cargo build --bin backend --release
+fi
+
