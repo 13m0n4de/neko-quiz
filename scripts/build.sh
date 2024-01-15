61e46da48df59a1cb8dcfafc036e1b0696ce4a16
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-01-15 22:09:23 +0800

    :hammer: 删除不必要参数

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,7 +6,7 @@ IFS=$'\n\t'
 TARGET=${1:-}
 
 pushd frontend
-CARGO_TARGET_DIR=../target-trunk trunk build --release --public-url /
+CARGO_TARGET_DIR=../target-trunk trunk build --release
 popd
 
 if [[ -n "$TARGET" ]]; then
