9a8c6b5b7db538450b0f681eeca1ed426233d644
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-08-27 05:38:39 +0800

    :recycle: refactor(shuttle): 使用 `CustomError` 错误类型

--- server/shuttle/src/main.rs
+++ server/shuttle/src/main.rs
@@ -1,13 +1,15 @@
-use std::{fs, path::Path};
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
 
 use api::{build_router, Config};
+use shuttle_runtime::CustomError;
 
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let config_str =
-        fs::read_to_string(Path::new("config.toml")).expect("Unable to read config file");
-    let config: Config = toml::from_str(&config_str).expect("Unable to parse config file");
-
-    let app = build_router(config, "./shuttle-dist/");
+    let config_str = fs::read_to_string(Path::new("config.toml")).map_err(CustomError::new)?;
+    let config: Config = toml::from_str(&config_str).map_err(CustomError::new)?;
+    let app = build_router(config, PathBuf::from("./shuttle-dist/"));
     Ok(app.into())
 }
