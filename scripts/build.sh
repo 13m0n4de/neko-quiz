c60c161dc7a2ff55863b93323aaf960fe70ea9a8
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-08-27 07:25:30 +0800

    :recycle: refactor(frontend, scripts): 使用 Trunk 的链接属性启用 wasm-opt 优化

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,18 +9,6 @@ pushd frontend
 CARGO_TARGET_DIR=../target-trunk trunk build --release
 popd
 
-WASM_FILES=(dist/*.wasm)
-if [ ${#WASM_FILES[@]} -eq 0 ]; then
-    echo "Warning: No WASM files found in dist/. Skipping optimization."
-else
-    for WASM_FILE in "${WASM_FILES[@]}"; do
-        echo "Optimizing WASM file: $WASM_FILE"
-        wasm-opt -Oz -o "${WASM_FILE}.opt" "$WASM_FILE"
-        mv "${WASM_FILE}.opt" "$WASM_FILE"
-        echo "Optimization complete for $WASM_FILE"
-    done
-fi
-
 if [[ -n "$TARGET" ]]; then
     cargo build --target "$TARGET" --bin server --release
 else
